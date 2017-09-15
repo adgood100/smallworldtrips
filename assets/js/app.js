26f6a85742017561aeb2553eec83f206c7fdeacc
Gage LaVergne <gagelavergne@Gages-MacBook-Pro.local>
2017-09-15 18:33:14 -0500

    latest layout branch

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,15 @@ $( document ).ready(function(){
  $(".button-collapse").sideNav();
 
  $('.slider').slider({
- 	indicators: false
- });
+ 	indicators: false,
+ 	height: 550
+ 	});
 
-
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".nav-switch");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
  
 });
